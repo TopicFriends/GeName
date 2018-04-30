1de64efd42dfed0f2a3a0a6131859653eb487e48
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2018-04-30 22:51:21 +0200

    defaultNameGenParams

--- gename/src/app/core/name-gen.service.ts
+++ gename/src/app/core/name-gen.service.ts
@@ -1,22 +1,49 @@
 import { Injectable } from '@angular/core';
 
-export class NameGenParams {
-  syllables: Range
-  syllableChars: Range
-  chars: Range
+export class Range {
+  constructor(
+    public min: number,
+    public maxInclusive: number,
+  ) {}
 }
-
 
 export const substitutions = [
   'ch k ck q',
   's z',
-  'f ph'
+  'f ph',
+  'o u oo',
 ]
 
-export const endings = 'ity ia on or is tron'
+export const prefixes = 'pro flex'
+
+/** https://en.wikipedia.org/wiki/Suffix */
+export const endings = 'ity ia on or is tron ing'
 
 
 export const vowels = 'aeiouy'
+
+export class NameGenParams {
+  syllables: Range
+  syllableChars: Range
+  chars: Range
+  consonantsInRow: Range
+  vowelsInRow: Range
+  charsOverlapBetweenWords: Range
+}
+
+export function range(number: number, number2: number) {
+  return new Range(number, number2)
+}
+
+export const defaultNameGenParams: NameGenParams = {
+  syllables: range(1, 3),
+  syllableChars: range(1, 4),
+  chars: range(3, 7),
+  consonantsInRow: range(1, 2),
+  vowelsInRow: range(1, 3),
+  charsOverlapBetweenWords: range(1, 4),
+}
+
 
 @Injectable()
 export class NameGenService {
